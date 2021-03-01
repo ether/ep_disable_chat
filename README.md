1154fcd359540f8920fad9c7a7518bb5487fe80d
John McLear <john@mclear.co.uk>
2021-03-01 15:46:36 +0000

    First Korma-it

---
+++ README.md
@@ -0,0 +1,5 @@
+![Publish Status](https://github.com/ether/ep_disable_chat/workflows/Node.js%20Package/badge.svg) ![Backend Tests Status](https://github.com/ether/ep_disable_chat/workflows/Backend%20tests/badge.svg)
+
+# Disable Chat Etherpad
+
+Disables Chat and the Settings to show chat.
---
+++ disable_chat.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.eejsBlock_styles  = (hookName, context, cb) => {
+  context.content += `<style>
+    .hide-for-mobile { display:none !important; }
+    #chaticon, #chatbox { display: none !important; }
+    #users { height:100%; }
+    .popup#users.chatAndUsers > .popup-content { height:100%; }
+    .popup#users.chatAndUsers { height:100%; }
+  </style>`;
+  cb();
+};
